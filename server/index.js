f42d9dc7ac499cede9877b07136a3364ef9ccf1c
Andre Sampaio <andremicael@gmail.com>
2024-07-08 21:39:59 -0400

    server voltou a funcionar

--- server/index.js
+++ server/index.js
@@ -1,35 +1,76 @@
-{
-    "version": 2,
-    "builds": [
-      { "src": "./index.js", "use": "@vercel/node" }
-    ],
-    "rewrites": [
-      { 
-        "source": "/stock/:symbol", 
-        "destination": "/index.js"
-      },
-      {
-        "source": "/(.*)", 
-        "destination": "/index.js"
-      }
-    ],
-    "headers": [
-      {
-        "source": "/stock/:symbol",
-        "headers": [
-          { "key": "Access-Control-Allow-Origin", "value": "https://stock-wall.vercel.app" },
-          { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-        ]
-      },
-      {
-        "source": "/(.*)",
-        "headers": [
-          { "key": "Access-Control-Allow-Origin", "value": "*" },
-          { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-        ]
-      }
-    ]
-  }
-  
+const puppeteer = require('puppeteer');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Configurar CORS para permitir qualquer origem
+app.use(cors());
+
+const port = process.env.PORT || 3001;
+
+// Rota para a raiz ("/")
+app.get('/', (req, res) => {
+    res.send('Bem-vindo ao serviço de consulta de ações!');
+});
+
+app.get('/stock/:symbol', async (req, res) => {
+    const symbol = req.params.symbol;
+    const url = `https://fundamentus.com.br/detalhes.php?papel=${symbol}`;
+    const urlProv = `https://fundamentus.com.br/proventos.php?papel=${symbol}`;
+
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const result = await page.evaluate(() => {
+        const empresa = document.querySelector("body > div.center > div.conteudo.clearfix > table:nth-child(2) > tbody > tr:nth-child(3) > td:nth-child(2)");
+        const setor = document.querySelector("body > div.center > div.conteudo.clearfix > table:nth-child(2) > tbody > tr:nth-child(4) > td:nth-child(2)");
+        const element = document.querySelector('.data.destaque.w3');
+        const lpa = document.querySelector("body > div.center > div.conteudo.clearfix > table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(6)");
+        const vpa = document.querySelector("body > div.center > div.conteudo.clearfix > table:nth-child(4) > tbody > tr:nth-child(3) > td:nth-child(6) > span");
+
+        if (element && lpa && vpa && empresa && setor) {
+            return {
+                empresa: empresa.innerText,
+                element: element.innerText,
+                lpa: lpa.innerText,
+                vpa: vpa.innerText,
+                setor: setor.innerText,
+            };
+        } else {
+            return 'N/A';
+        }
+    });
+
+    await page.goto(urlProv);
+
+    const proventos = await page.evaluate(() => {
+        // Adicione a função toggleClasse() se necessário
+        if (typeof toggleClasse === 'function') {
+            toggleClasse();
+        }
+
+        // Seleciona todas as linhas dentro do tbody
+        const rows = document.querySelectorAll('#resultado-anual tbody tr');
+
+        // Armazena os resultados em um array
+        const provs = [];
+
+        rows.forEach(row => {
+            const cells = row.querySelectorAll('td');
+            const ano = cells[0]?.textContent || 'N/A';
+            const valor = cells[1]?.textContent || 'N/A';
+            provs.push({ ano, valor });
+        });
+
+        return provs;
+    });
+
+    await browser.close();
+
+    res.json({ stock: symbol, price: result, proventos: proventos });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
